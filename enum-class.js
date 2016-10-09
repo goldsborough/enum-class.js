be52005d98163066531473076cc4502d9b715a86
goldsborough <peter@goldsborough.me>
2016-10-09 02:33:09 +0100

    Initial commit

---
+++ enum-class.js
@@ -0,0 +1,458 @@
+/**
+* @file Defines the `EnumClass` type.
+* @author Peter Goldsborough <peter@goldsborough.me>
+* @license MIT
+*/
+
+/**
+ * An error thrown for permission errors.
+ */
+class EnumClassPermissionError extends Error {
+  constructor() {
+    super('No permission to call enum constructor');
+  }
+}
+
+/**
+ * An error thrown for definition errors.
+ */
+class EnumClassDefinitionError extends Error {
+  constructor() {
+    super('No permission to call enum constructor');
+  }
+}
+
+/**
+ * A symbol for the name enum property.
+ * @type {Symbol}
+ */
+const _name = Symbol.for('enum.name');
+
+/**
+ * A symbol for the ordinal enum property.
+ * @type {Symbol}
+ */
+const _ordinal = Symbol.for('enum.ordinal');
+
+/**
+ * A symbol for the value enum property.
+ * @type {Symbol}
+ */
+const _value = Symbol.for('enum.value');
+
+/**
+ * A permission key symbol available only inside this module.
+ * @type {Symbol}
+ */
+const _permission = Symbol.for(Math.random());
+
+/**
+ * The base class for all user-defined enums.
+ */
+class BaseEnumClass {
+  constructor(name, ordinal, value, permission) {
+    if (permission !== _permission) {
+      throw new EnumClassPermissionError();
+    }
+
+    if (typeof name !== 'string') {
+      throw new EnumClassDefinitionError('Name member must be a string');
+    }
+
+    /**
+     * The name of the enum class member.
+     * @type {string}
+     * @constant
+     */
+    this[_name] = name;
+
+    /**
+     * The unique ordinal of the enum class member.
+     * @type {number}
+     * @constant
+     */
+    this[_ordinal] = ordinal;
+
+    /**
+     * The value of the enum class member.
+     * @type {any}
+     * @constant
+     */
+    this[_value] = value;
+
+    // Immutable forever
+    Object.freeze(this);
+  }
+
+  /**
+   * @return {string} The name property of the enum class member
+   */
+  get name() {
+    return this[_name];
+  }
+
+  /**
+   * @return {number} The ordinal property of the enum class member.
+   */
+  get ordinal() {
+    return this[_ordinal];
+  }
+
+  /**
+   * @return {any} The value property of the enum class member.
+   */
+  get value() {
+    return this[_value];
+  }
+
+  /**
+   * @return {string} The name of the enum class of
+   *                  which this instance is a member.
+   */
+  get type() {
+    return this.constructor.name;
+  }
+
+  /**
+   * @return {string} The name of the member.
+   */
+  toString() {
+    return this.name;
+  }
+}
+
+/**
+ * A symbol for the members property of the `EnumClassIterator`.
+ * @type {Symbol}
+ */
+const _members = Symbol.for('members');
+
+/**
+ * A symbol for the index property of the `EnumClassIterator`.
+ * @type {Symbol}
+ */
+const _index = Symbol.for('index');
+
+/**
+ * An iterator over an enum class.
+ */
+class EnumClassIterator {
+  /**
+   * Constructs the `EnumClassIterator` from an `EnumClass`.
+   *
+   * @param {class} enumClass An `EnumClass`.
+   */
+  constructor(enumClass) {
+    this[_members] = enumClass.members;
+    this[_index] = 0;
+  }
+
+  /**
+   * Tests if the iterator is exhausted.
+   * @return {boolean} True if the iterator is exhausted, else false.
+   */
+  get done() {
+    return this[_index] === this[_members].length;
+  }
+
+  /**
+   * @return {object} The state of the iterator, as per the iterator protocol.
+   */
+  next() {
+    if (this.done) return {done: true};
+    return {
+      done: false,
+      value: this[_members][this[_index]++]
+    };
+  }
+}
+
+/**
+ * Tests if an array contains only strings.
+ * @param  {array} array The array to test.
+ * @return {boolean} True if the array contains only strings, else false.
+ */
+function containsOnlyStrings(array) {
+  // So we can also pass `arguments`
+  return Array.prototype.every.call(array, element => {
+    return typeof element === 'string';
+  });
+}
+
+/**
+ * Turns an array into an object suitable for enum creation.
+ *
+ * The object maps names to values, in this case all `undefined`.
+ *
+ * @param  {Array} array The array to turn into an object.
+ * @return {object} An object with `(string, undefined)` pairs.
+ */
+function turnIntoObject(array) {
+  const object = {};
+
+  // So we can also pass `arguments`
+  // eslint-disable-next-line no-return-assign
+  Array.prototype.forEach.call(array, name => {
+    object[name] = undefined;
+  });
+
+  return object;
+}
+
+/**
+ * Tests if an array contains only unique keys.
+ * @param  {array} array The array to test.
+ * @return {boolean} True if the array is unique, else false.
+ */
+function allAreUnique(array) {
+  const set = {};
+
+  for (let index = 0, stop = array.length; index < stop; ++index) {
+    if (array[index] in set) return false;
+    set[array[index]] = true;
+  }
+
+  return true;
+}
+
+/**
+ * Attempts to turn an array of members into an object.
+ *
+ * The array must contain only strings and all must be unique.
+ *
+ * @param  {array} array An array of strings.
+ * @return {object} An object with `(string, undefined)` pairs.
+ */
+function tryToMakeMemberObject(array) {
+  if (containsOnlyStrings(array) && allAreUnique(array)) {
+    return turnIntoObject(array);
+  } else {
+    throw new TypeError('Invalid format for enum members');
+  }
+}
+
+/**
+ * Parses the members argument and ensures their correct format.
+ *
+ * Allowed formats are:
+ * // 'A', 'B', 'C'
+ * // 'A, B, C'
+ * // 'A B C'
+ * // ['A', 'B', 'C']
+ * // {A: 1, B: 2}
+ *
+ * @param  {string|object|array} members The members.
+ * @return {[type]}         [description]
+ */
+function parseMembers(members) {
+  if (arguments.length > 1) {
+    // 'A', 'B', 'C'
+    return tryToMakeMemberObject(arguments);
+  } else if (typeof members === 'string') {
+    // 'A, B, C' or 'A B C'
+    let match = members.match(/\w+/g);
+    if (match && allAreUnique(match)) {
+      return turnIntoObject(match);
+    }
+  } else if (Array.isArray(members)) {
+    // ['A', 'B', 'C']
+    return tryToMakeMemberObject(members);
+  } else if (typeof members === 'object') {
+    return members;
+  }
+
+  throw new TypeError('Invalid format for enum members');
+}
+
+/**
+ * Instantiates enum members from an object with `(name: value)` pairs.
+ * @param {class} enumClass An enum class.
+ * @param  {object} members An object of `(name: value)` pairs.
+ * @return {array} An array of enum instances.
+ */
+function instantiateMembers(enumClass, members) {
+  const instances = [];
+
+  for (let name in members) {
+    if (members.hasOwnProperty(name)) {
+      instances.push(enumClass.create(name, members[name]));
+    }
+  }
+
+  return instances;
+}
+
+/**
+ * Creates a new enum with the given name.
+ *
+ * Enums all have a name, an ordinal and a value.
+ *
+ * @param  {string} name The name for the enum to create.
+ * @param {class} Base The base class from which to inherit the enum class.
+ * @return {class} A new enum class object.
+ */
+function createEnumClass(name, Base) {
+  const body = `return class ${name} extends Base {}`;
+  // eslint-disable-next-line no-new-func
+  return (new Function('Base', body))(Base);
+}
+
+/**
+ * Defines and finalizes the enum class given its members.
+ * @param  {string} name The name of the enum class to create.
+ * @param  {array} members   An array of instances of the enum class.
+ * @return {[type]}           [description]
+ */
+function defineEnumClass(name) {
+  const enumClass = createEnumClass(name, BaseEnumClass);
+
+  /**
+   * An array of the members of the enum class.
+   * @type {array}
+   */
+  let members = [];
+
+  /**
+   * The length (number of members) of the enum class.
+   * @type {number}
+   */
+  let length = 0;
+
+  /**
+   * Factory function for new enum class members.
+   * @param {string} name  The name of the member.
+   * @param {any} value The value of the member.
+   * @return {EnumClass} The new member.
+   */
+  enumClass.create = function(name, value) {
+    if (enumClass.contains(name)) {
+      throw new EnumClassDefinitionError(
+        'Attempted to create duplicate' +
+        `enum class member '${name}'`
+      );
+    }
+
+    // Allow passing { name: value } objects.
+    if (typeof name === 'object') {
+      const object = name;
+      name = Object.keys(name)[0];
+      value = object[name];
+    }
+
+    // eslint-disable-next-line new-cap
+    const member = new enumClass(name, length, value, _permission);
+
+    length += 1;
+    members.push(member);
+    enumClass[member.name] = member;
+
+    return member;
+  };
+
+  /**
+   * Defines a public, read-only length property mirroring the private length.
+   */
+  Object.defineProperty(enumClass, 'length', {
+    get: function() { return length; }
+  });
+
+  /**
+   * Defines a public, read-only members property mirroring the private length.
+   */
+  Object.defineProperty(enumClass, 'members', {
+    get: function() { return Object.seal(members.slice()); }
+  });
+
+  /**
+   * Defines a public, read-only array of the enum class' members' names.
+   * @type {[type]}
+   */
+  Object.defineProperty(enumClass, 'names', {
+    get: (function() {
+      let names = null;
+      return function() {
+        if (!names) {
+          names = members.map(member => member.name);
+        }
+        return names;
+      };
+    })()
+  });
+
+  /**
+   * Returns the member with the given ordinal.
+   * @param  {number} ordinal The ordinal for which to get the member.
+   * @return {EnumClass} The member with that ordinal (if any exists).
+   */
+  enumClass.at = function(ordinal) {
+    return this.members[ordinal];
+  };
+
+  /**
+   * Tests if the given argument is a member of this enum class.
+   *
+   * If the argument is a string, the method tests  if any member has such a
+   * name. Else, it performs membership test just like `isMember()`.
+   *
+   * @param  {string|EnumClass} member The member to test for.
+   * @return {boolean} True if the argument is a member, else false.
+   * @see enumClass.isMember
+   */
+  enumClass.contains = function(member) {
+    if (typeof member === 'string') {
+      return member in this;
+    } else {
+      return this.isMember(member);
+    }
+  };
+
+  /**
+   * Tests if the given object is a member of this enum class.
+   *
+   * Equialent to `member instanceof <class>`.
+   *
+   * @param  {object}  member An object to test membership for.
+   * @return {Boolean}        [description]
+   * * @see enumClass.contains
+   */
+  enumClass.isMember = function(member) {
+    return member instanceof this;
+  };
+
+  /**
+   * Returns an iterator following the iterator protocol.
+   *
+   * This allows iteration of the enum's members via `for ... of` loops.
+   * @see [Iterator Protocol on MDN]{@link https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Iteration_protocols}
+   *
+   * @return {EnumClassIterator} An iterator over the enum class.
+   */
+  enumClass[Symbol.iterator] = function() {
+    return new EnumClassIterator(this);
+  };
+
+  return enumClass;
+}
+
+/**
+ * creates a new enum class.
+ *
+ * This will create a class with static members of its own type.
+ * The members are derived from the argument(s) after `name`,
+ * which can be one of several formats (see `parseMembers`).
+ *
+ * @param {name} name The name of the enum to create.
+ * @param {string|array|object} Members as further arguments.
+ * @return {class} A new class.
+ */
+export default function EnumClass(name) {
+  const enumClass = defineEnumClass(name);
+
+  const args = Array.prototype.slice.call(arguments, 1);
+  if (args.length > 0) {
+    const members = parseMembers.apply(null, args);
+    instantiateMembers(enumClass, members);
+  }
+
+  return enumClass;
+}
---
+++ test.js
@@ -0,0 +1,178 @@
+import test from 'ava';
+import EnumClass from './enum-class.js';
+
+/* eslint-disable new-cap */
+
+test('parses correct formats well', t => {
+  const hasOwn = Object.hasOwnProperty;
+
+  let E = EnumClass('E', 'X, Y, Z');
+  let expected = {X: [0, undefined], Y: [1, undefined], Z: [2, undefined]};
+  for (let name in expected) {
+    if (hasOwn.call(expected, name)) {
+      t.is(E[name].name, name);
+      t.is(E[name].ordinal, expected[name][0]);
+      t.is(E[name].value, expected[name][1]);
+    }
+  }
+
+  E = EnumClass('E', 'X Y Z');
+  for (let name in expected) {
+    if (hasOwn.call(expected, name)) {
+      t.is(E[name].name, name);
+      t.is(E[name].ordinal, expected[name][0]);
+      t.is(E[name].value, expected[name][1]);
+    }
+  }
+
+  E = EnumClass('E', ['X', 'Y', 'Z']);
+  for (let name in expected) {
+    if (hasOwn.call(expected, name)) {
+      t.is(E[name].name, name);
+      t.is(E[name].ordinal, expected[name][0]);
+      t.is(E[name].value, expected[name][1]);
+    }
+  }
+
+  E = EnumClass('E', 'X', 'Y', 'Z');
+  for (let name in expected) {
+    if (hasOwn.call(expected, name)) {
+      t.is(E[name].name, name);
+      t.is(E[name].ordinal, expected[name][0]);
+      t.is(E[name].value, expected[name][1]);
+    }
+  }
+
+  const f = function() { };
+  E = EnumClass('E', {
+    X: f,
+    Y: 'foo',
+    Z: 123
+  });
+
+  expected = {X: [0, f], Y: [1, 'foo'], Z: [2, 123]};
+
+  for (let name in expected) {
+    if (hasOwn.call(expected, name)) {
+      t.is(E[name].name, name);
+      t.is(E[name].ordinal, expected[name][0]);
+      t.is(E[name].value, expected[name][1]);
+    }
+  }
+});
+
+test('throws for incorrect formats', t => {
+  t.throws(() => { EnumClass(123); });
+  t.throws(() => { EnumClass('#$%@$%#$'); });
+});
+
+test('throws for duplicate members', t => {
+  t.throws(() => EnumClass('E', 'X, X'));
+
+  // t.throws(() => {
+  //   const E = EnumClass('E', 'X');
+  //   E.create('X');
+  // });
+});
+
+test('members of different classes compare false', t => {
+  const E = EnumClass('E', 'X, Y, Z');
+  const F = EnumClass('F', 'X, Y, Z');
+
+  for (let name of E.names) {
+    t.not(E[name], F[name]);
+  }
+
+  t.is(E.X, E.X);
+  t.is(E.X, E.X);
+});
+
+test('members are immutable', t => {
+  const E = EnumClass('E', 'X');
+
+  t.throws(() => {
+    E.X.ordinal = -1;
+  });
+
+  t.throws(() => {
+    E.X.name = 'asdfasfxasd';
+  });
+
+  t.throws(() => {
+    E.X.value = function() {};
+  });
+});
+
+test('classes are immutable', t => {
+  const E = EnumClass('E', 'X, Y, Z');
+
+  t.throws(() => {
+    E.members.push(1);
+  });
+
+  t.throws(() => {
+    E.length = -1;
+  });
+});
+
+test('throws when trying to instantiate directly', t => {
+  const E = EnumClass('E');
+  t.throws(() => new E('X', 1));
+});
+
+test('contains works', t => {
+  const members = 'X Y Z'.split(' ');
+  const E = EnumClass('E', members);
+  t.true(members.every(E.contains.bind(E)));
+});
+
+test('members can be added through create', t => {
+  const E = EnumClass('E');
+  t.is(E.length, 0);
+
+  E.create('X');
+  t.is(E.length, 1);
+  t.true(E.contains('X'));
+  t.is(E.X.name, 'X');
+  t.is(E.X.ordinal, 0);
+  t.is(E.X.value, undefined);
+
+  E.create('Y', 'foo');
+  t.is(E.length, 2);
+  t.true(E.contains('Y'));
+  t.is(E.Y.name, 'Y');
+  t.is(E.Y.ordinal, 1);
+  t.is(E.Y.value, 'foo');
+
+  E.create({Z: [1, 2, 3]});
+  t.is(E.length, 3);
+  t.true(E.contains('Z'));
+  t.is(E.Z.name, 'Z');
+  t.is(E.Z.ordinal, 2);
+  t.deepEqual(E.Z.value, [1, 2, 3]);
+});
+
+test('iteration works', t => {
+  const E = EnumClass('E', 'X, Y, Z');
+  const expected = [E.X, E.Y, E.Z];
+
+  let index = 0;
+  for (let member of E) {
+    t.is(member, expected[index++]);
+  }
+});
+
+test('toString prints the name', t => {
+  const E = EnumClass('E', 'X');
+  t.is(E.X.toString(), 'X');
+});
+
+test('length stays up to date', t => {
+  const E = EnumClass('E', 'X, Y, Z');
+  t.is(E.length, 3);
+
+  for (let i = 1; i <= 100; ++i) {
+    E.create(String(i));
+    t.is(E.length, 3 + i);
+  }
+});
